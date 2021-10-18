5d0b0113de43127630d6865be30a13dd29eb18c5
antjamespadgett <padgettanthonyj@gmail.com>
2021-10-17 19:40:43 -0500

    added pratice work

--- Program.cs
+++ Program.cs
@@ -12,13 +12,20 @@ namespace cSharpPractice
 
             // Console.ReadLine();
 
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            /*  Console.WriteLine("   /|");
+              Console.WriteLine("  / |");
+              Console.WriteLine(" /  |");
+              Console.WriteLine("/___|");
+
+              Console.ReadLine();*/
+
+            Console.WriteLine("There once was a man named George");
+            Console.WriteLine("He was 70 years old");
+            Console.WriteLine("He really like the name George");
+            Console.WriteLine("But he didnt like being 70");
+
 
             Console.ReadLine();
-
 
 
             
